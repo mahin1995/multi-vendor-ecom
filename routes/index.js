013e06d43664bc570c85086d508fc19799fa549f
Mohaimanul Islam Mahin <mohaimanul6@gmail.com>
2022-04-07 16:43:04 +0600

    from mahin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ECOM Home' });
+});
+router.get('/wishlist', function(req, res, next) {
+  res.render('wishlist', { title: 'wishlist' });
+});
+router.get('/cart', function(req, res, next) {
+  res.render('cart', { title: 'cart' });
+});
+router.get('/checkout', function(req, res, next) {
+  res.render('checkout', { title: 'checkout' });
 });
 
 module.exports = router;
