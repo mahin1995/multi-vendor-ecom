aebcf9fb39d8b2496c1f8156ea3d51992c4ec7ff
mahin1995 <mohimanul8@gmail.com>
2022-04-19 23:23:46 +0600

    f

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.render('account', { title: 'Login' });
-
+  res.render('login', { title: 'Login' });
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'signup' });
 });
 
 module.exports = router;
